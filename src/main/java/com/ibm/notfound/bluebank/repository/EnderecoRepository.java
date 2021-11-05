6f976c4dfa00c9b9d469b27479a2acf31a575b86
paulocfj <paulo.cfj2@gmail.com>
2021-11-04 22:58:04 -0300

    Adicionado repositório para endereco

---
+++ src/main/java/com/ibm/notfound/bluebank/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.ibm.notfound.bluebank.repository;
+
+import com.ibm.notfound.bluebank.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public  interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
